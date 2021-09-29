306d33a9546dbdac8724638425f813579b90d1c3
WWW-max <464711201@qq.com>
2021-09-29 17:10:19 +0800

    单文件组件

---
+++ School.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">
+      <h1>学校名称：{{schoolName}}</h1>
+      <h1>学校地址：{{address}}</h1>
+      <button @click="show">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+export default {
+        name:'School',
+        data(){
+            return {
+                schoolName:'清华大学',
+                address:'北京'
+            }
+        },
+        methods: {
+            show(){
+                 alert(this.schoolName);
+            }
+           
+        },
+}
+</script>
+
+<style>
+.demo{
+    background-color: greenyellow;
+}
+</style>
---
+++ Student.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>学生姓名：{{studentName}}</h1>
+    <h1>学生年龄：{{age}}</h1>
+</div>
+</template>
+<script>
+     export default{
+         name:'Student',
+         data(){
+             return {
+                 studentName:'张三',
+                 age:'24'
+             }
+         }
+     }
+</script>
+<style>
+
+</style>
