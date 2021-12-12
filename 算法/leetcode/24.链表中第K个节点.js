40b14c3b93afbbe7c2d3439d64b1883c4c7877a0
WWW-max <464711201@qq.com>
2021-12-12 21:54:27 +0800

    24.链表中第K个节点.js

---
+++ 算法/leetcode/24.链表中第K个节点.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+ var getKthFromEnd = function(head, k) {
+    let node = head, n = 0;
+    while (node) {
+        node = node.next;
+        n++;
+    }
+    node = head;
+    for (let i = 0; i < n - k; i++) {
+        node = node.next;
+    }
+    return node; 
+};
